672cf77ab6174f7eacd49c430b26aa8dd3ae59c2
ayeshaasghar109 <asgharayesha173@gmail.com>
2026-09-05 11:19:55 +0500

    Use CPU-only PyTorch to fix slow Azure cold starts

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ init_db()
 app=FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
